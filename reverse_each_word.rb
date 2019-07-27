859abad4d6a243166e4b7007ca0f64054e4be28b
Emily G <aspentree79@gmail.com>
2019-07-27 01:31:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
-original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
+array_one = string.split(" ")
+  new_array = []
+  array_one.each do|string|
+    new_array << string.reverse
   end
-  return_array.join(" ")
+  new_array.join(" ")
 end
 
 def reverse_each_word(string)

14d172b94e09319527ba74e2bb8f6bbfef81ebe7
Emily G <aspentree79@gmail.com>
2019-07-27 01:25:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,9 @@
 def reverse_each_word(string)
-(string.split).map { |word| word.reverse! }
-
+# (string.split).map { |word| word.reverse! }
+original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
